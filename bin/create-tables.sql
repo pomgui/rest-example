bc039e216fee5dd5b87d796675e308e44bab04f0
wpomier <55899540+pomgui@users.noreply.github.com>
2021-12-06 10:55:58 -0300

    Added a database function to calculate distances between two coordinates

--- bin/create-tables.sql
+++ bin/create-tables.sql
@@ -35,3 +35,27 @@ CREATE TABLE station_books (
 ALTER TABLE station_books ADD CONSTRAINT fk_station FOREIGN KEY (station_id) REFERENCES stations(id);
 
 COMMIT WORK;
+
+SET AUTODDL OFF;
+SET TERM ^ ;
+
+-- Function to calculate the distance between two geo coordenates
+-- based on https://github.com/manuelbieh/geolib/blob/master/src/getDistance.ts 
+CREATE OR ALTER FUNCTION f_geo_distance(lat1 float, lon1 float, lat2 float, lon2 float) RETURNS float AS
+    DECLARE val float;
+BEGIN
+    lat1 = lat1 * PI() / 180;
+    lat2 = lat2 * PI() / 180;
+    lon1 = lat1 * PI() / 180;
+    lon2 = lat2 * PI() / 180;
+
+    val = sin(lat2) * sin(lat1) + cos(lat2) * cos(lat1) * cos(lon1 - lon2);
+    if(val>1)  THEN val=1;
+    if(val<-1) THEN val=-1;
+
+    RETURN acos(val) * 6378137; -- earth radius
+END^
+
+SET TERM ; ^
+COMMIT WORK;
+SET AUTODDL ON;
